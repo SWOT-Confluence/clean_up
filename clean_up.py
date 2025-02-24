20e7ab9e8d6c756eeb54f26aab4448b419b7319d
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-02-24 14:59:44 -0500

    Refactor to upload config files for data retention and remove S3 deletion

--- clean_up.py
+++ clean_up.py
@@ -5,6 +5,7 @@ import logging
 import os
 import pathlib
 import shutil
+import tempfile
 
 # Third-party imports
 import boto3
@@ -16,67 +17,79 @@ logging.basicConfig(format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(messa
                     datefmt='%Y-%m-%dT%H:%M:%S',
                     level=logging.INFO)
 
-class CleanUp:
-    """Class to clean up after Confluence workflow execution."""
 
-    EFS_DIRS = [
-        pathlib.Path("/mnt/input"),
-        pathlib.Path("/mnt/flpe"),
-        pathlib.Path("/mnt/moi"),
-        pathlib.Path("/mnt/diagnostics"),
-        pathlib.Path("/mnt/offline"),
-        pathlib.Path("/mnt/validation"),
-        pathlib.Path("/mnt/output"),
-        pathlib.Path("/mnt/logs")
-    ]
-    S3 = boto3.client("s3")
+COPY_FILES = [
+    "continent-setfinder.json",
+    "prediagnostics_config.R",
+    "reaches_of_interest.json"
+]
+EFS_DIRS = [
+    pathlib.Path("/mnt/input"),
+    pathlib.Path("/mnt/flpe"),
+    pathlib.Path("/mnt/moi"),
+    pathlib.Path("/mnt/diagnostics"),
+    pathlib.Path("/mnt/offline"),
+    pathlib.Path("/mnt/validation"),
+    pathlib.Path("/mnt/output"),
+    pathlib.Path("/mnt/logs")
+]
+S3 = boto3.client("s3")
 
-    def __init__(self, prefix):
-        """
-        Parameters:
-        prefix (str): Indicates the AWS environment venue
-        """
 
-        self.s3_map = f"{prefix}-map-state"
+def delete_efs():
+    """Remove all files from the EFS."""
 
-    def efs(self):
-        """Remove all files from the EFS."""
+    for efs_dir in EFS_DIRS:
+        try:
+            with os.scandir(efs_dir) as entries:
+                for entry in entries:
+                    if entry.is_file():
+                        os.unlink(entry.path)
+                    else:
+                        shutil.rmtree(entry.path)
+                    logging.info("Removed %s.", entry.path)
+        except OSError as e:
+            logging.error(e)
+            logging.info("Could not delete files in %s.", efs_dir)
 
-        for efs_dir in self.EFS_DIRS:
-            try:
-                with os.scandir(efs_dir) as entries:
-                    for entry in entries:
-                        if entry.is_file():
-                            os.unlink(entry.path)
-                        else:
-                            shutil.rmtree(entry.path)
-                        logging.info("Removed %s.", entry.path)
-            except OSError as e:
-                logging.error(e)
-                logging.info("Could not delete files in %s.", efs_dir)
-
-    def s3(self):
-        """Remove all files from S3 Map State bucket."""
-
-        paginator = self.S3.get_paginator('list_objects_v2')
-        page_iterator = paginator.paginate(
-            Bucket=self.s3_map
-        )
-        key_count = list(page_iterator)[0]["KeyCount"]
-        if key_count > 0:
-            items = { "Objects": [ {"Key": key["Key"] } for page in page_iterator for key in page["Contents"]]}
-            self.S3.delete_objects(Bucket=self.s3_map, Delete=items)
-            for item in items["Objects"]: logging.info("Deleted s3://%s/%s", self.s3_map, item["Key"])
+def copy_s3(config_bucket, json_bucket, bucket_key):
+    """Copy config files over to JSON bucket to preserve run data."""
+    with tempfile.TemporaryDirectory() as temp_dir:
+        data_dir = pathlib.Path(temp_dir)
+        for s3_file in COPY_FILES:
+            S3.download_file(
+                config_bucket,
+                s3_file,
+                data_dir.joinpath(s3_file)
+            )
+            S3.upload_file(
+                data_dir.joinpath(s3_file),
+                json_bucket,
+                f"{bucket_key}/{s3_file}",
+                ExtraArgs={"ServerSideEncryption": "AES256"}
+            )
+            logging.info("Copied s3://%s/%s/%s.", json_bucket, bucket_key, s3_file)
 
 def create_args():
     """Create and return argparser with arguments."""
 
     arg_parser = argparse.ArgumentParser(description="Redrive Confluence Step Function failures")
-    arg_parser.add_argument("-p",
-                            "--prefix",
+    arg_parser.add_argument("-d",
+                            "--delete",
+                            help="Indicate EFS files should be deleted.",
+                            action="store_true")
+    arg_parser.add_argument("-c",
+                            "--configbucket",
                             type=str,
-                            default="confluence-dev1",
-                            help="Prefix that indicates AWS environment venue.")
+                            help="Name of config S3 bucket to copy JSON files from.")
+    arg_parser.add_argument("-j",
+                            "--jsonbucket",
+                            type=str,
+                            help="Name of S3 bucket to copy JSON files to.")
+    arg_parser.add_argument("-k",
+                            "--bucketkey",
+                            type=str,
+                            help="Name of prefix to upload JSON files to.")
     return arg_parser
 
 def run_clean_up():
@@ -85,11 +98,22 @@ def run_clean_up():
     start = datetime.datetime.now()
     arg_parser = create_args()
     args = arg_parser.parse_args()
-    prefix = args.prefix
-    logging.info("Prefix: %s", prefix)
-    clean_up = CleanUp(prefix)
-    clean_up.efs()
-    clean_up.s3()
+
+    delete = args.delete
+    config_bucket = args.configbucket
+    json_bucket = args.jsonbucket
+    bucket_key = args.bucketkey
+
+    logging.info("Delete: %s", delete)
+    logging.info("Config S3 bucket: %s", config_bucket)
+    logging.info("JSON S3 bucket: %s", json_bucket)
+    logging.info("S3 bucket key: %s", bucket_key)
+
+    if delete:
+        delete_efs()
+
+    copy_s3(config_bucket, json_bucket, bucket_key)
+
     end = datetime.datetime.now()
     logging.info("Elapsed time: %s", end - start)
 
