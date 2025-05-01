dd8c674b19bfb6432dd108d8f86332a8fc334005
Joshua Garde <jgarde@jpl.nasa.gov>
2025-05-01 13:08:10 -0700

    Initial terraform module refactor

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,53 @@
- terraform {
-  backend "s3" {
-    encrypt = true
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  default_tags {
-    tags = local.default_tags
-  }
-  region  = var.aws_region
-}
-
+# Data sources
 data "aws_caller_identity" "current" {}
 
+data "aws_cloudwatch_log_group" "cw_log_group" {
+  name = "/aws/batch/job/${var.prefix}-clean-up/"
+}
+
+data "aws_efs_file_system" "input" {
+  creation_token = "${var.prefix}-input"
+}
+
+data "aws_efs_file_system" "flpe" {
+  creation_token = "${var.prefix}-flpe"
+}
+
+data "aws_efs_file_system" "moi" {
+  creation_token = "${var.prefix}-moi"
+}
+
+data "aws_efs_file_system" "diagnostics" {
+  creation_token = "${var.prefix}-diagnostics"
+}
+
+data "aws_efs_file_system" "offline" {
+  creation_token = "${var.prefix}-offline"
+}
+
+data "aws_efs_file_system" "validation" {
+  creation_token = "${var.prefix}-validation"
+}
+
+data "aws_efs_file_system" "output" {
+  creation_token = "${var.prefix}-output"
+}
+
+data "aws_efs_file_system" "logs" {
+  creation_token = "${var.prefix}-logs"
+}
+
+data "aws_iam_role" "job" {
+  name = "${var.prefix}-batch-job-role"
+}
+
+data "aws_iam_role" "exec" {
+  name = "${var.prefix}-ecs-exe-task-role"
+}
+
+# Local variables
 locals {
-  account_id = sensitive(data.aws_caller_identity.current.account_id)
+  account_id = data.aws_caller_identity.current.account_id
   default_tags = length(var.default_tags) == 0 ? {
     application : var.app_name,
     environment : lower(var.environment),
@@ -35,4 +62,14 @@ module "confluence-clean-up" {
   aws_region        = var.aws_region
   environment       = var.environment
   prefix            = var.prefix
+  iam_execution_role_arn = data.aws_iam_role.exec.arn
+  iam_job_role_arn = data.aws_iam_role.job.arn
+  efs_file_system_ids = {
+    input = data.aws_efs_file_system.aws_efs_input.arn
+    flpe = data.aws_efs_file_system.flpe.arn
+    moi = data.aws_efs_file_system.moi.arn
+    diagnostics = data.aws_efs_file_system.diagnostics.arn
+    offline = data.aws_efs_file_system.offline.arn
+    logs = data.aws_efs_file_system.logs.arn
+  }
 }
--- terraform/modules/clean/confluence-clean-up.tf
+++ terraform/modules/clean/confluence-clean-up.tf
@@ -2,125 +2,108 @@
 resource "aws_batch_job_definition" "generate_batch_jd_clean_up" {
   name                  = "${var.prefix}-clean-up"
   type                  = "container"
-  container_properties  = <<CONTAINER_PROPERTIES
-  {
-    "image": "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-clean-up",
-    "executionRoleArn": "${data.aws_iam_role.exe_role.arn}",
-    "jobRoleArn": "${data.aws_iam_role.job_role.arn}",
-    "fargatePlatformConfiguration": { "platformVersion": "LATEST" },
-    "logConfiguration": {
-      "logDriver" : "awslogs",
-      "options": {
-        "awslogs-group" : "${data.aws_cloudwatch_log_group.cw_log_group.name}"
+
+  container_properties  = jsonencode({
+    image = "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-clean-up"
+    executionRoleArn = var.iam_job_role_arn
+    jobRoleArn = var.iam_job_role_arn
+    fargatePlatformConfiguration = {
+      platformVersion = "LATEST"
+    }
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group = aws_cloudwatch_log_group.cw_log_group.name
       }
-    },
-    "resourceRequirements": [
-      {"type": "MEMORY", "value": "512"},
-      {"type": "VCPU", "value": "0.25"}
-    ],
-    "mountPoints": [
+    }
+    resourceRequirements = [
       {
-        "sourceVolume": "input",
-        "containerPath": "/mnt/input",
-        "readOnly": false
+        type = "MEMORY"
+        value = "512"
       },
       {
-        "sourceVolume": "flpe",
-        "containerPath": "/mnt/flpe",
-        "readOnly": false
-      },
-      {
-        "sourceVolume": "moi",
-        "containerPath": "/mnt/moi",
-        "readOnly": false
-      },
-      {
-        "sourceVolume": "diagnostics",
-        "containerPath": "/mnt/diagnostics",
-        "readOnly": false
-      },
-      {
-        "sourceVolume": "offline",
-        "containerPath": "/mnt/offline",
-        "readOnly": false
-      },
-      {
-        "sourceVolume": "validation",
-        "containerPath": "/mnt/validation",
-        "readOnly": false
-      },
-      {
-        "sourceVolume": "output",
-        "containerPath": "/mnt/output",
-        "readOnly": false
-      },
-      {
-        "sourceVolume": "logs",
-        "containerPath": "/mnt/logs",
-        "readOnly": false
+        type = "VCPU",
+        value = "0.25"
       }
-    ],
-    "volumes": [
+    ]
+    mountPoints = [
       {
-        "name": "input",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_input.file_system_id}",
-          "rootDirectory": "/"
+        sourceVolume = "input",
+        containerPath = "/mnt/input"
+        readOnly = false
+      }, {
+        sourceVolume = "flpe"
+        containerPath = "/mnt/flpe"
+        readOnly = false
+      }, {
+        sourceVolume = "moi"
+        containerPath = "/mnt/moi"
+        readOnly = false
+      }, {
+        sourceVolume = "offline"
+        containerPath = "/mnt/offline"
+        readOnly = false
+      }, {
+        sourceVolume = "validation"
+        containerPath = "/mnt/validation"
+        readOnly = false
+      }, {
+        sourceVolume = "output"
+        containerPath = "/mnt/output"
+        readOnly = false
+      }, {
+        sourceVolume = "logs"
+        containerPath = "/mnt/logs"
+        readOnly = false
+      }
+    ]
+    volumes = [
+      {
+        name = "input"
+        efsVolumeConfiguration = {
+          fileSystemId = var.efs_file_system_ids["input"]
+          rootDirectory = "/"
         }
-      },
-      {
-        "name": "flpe",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_flpe.file_system_id}",
-          "rootDirectory": "/"
+      }, {
+        name = "flpe"
+        efsVolumeConfiguration = {
+          fileSystemId = var.efs_file_system_ids["flpe"]
+          rootDirectory = "/"
         }
-      },
-      {
-        "name": "moi",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_moi.file_system_id}",
-          "rootDirectory": "/"
+      }, {
+        name = "moi"
+        efsVolumeConfiguration = {
+          fileSystemId = var.efs_file_system_ids["moi"]
+          rootDirectory = "/"
         }
-      },
-      {
-        "name": "diagnostics",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_diagnostics.file_system_id}",
-          "rootDirectory": "/"
+      }, {
+        name = "diagnostics"
+        efsVolumeConfiguration = {
+          fileSystemId = var.efs_file_system_ids["diagnostics"]
+          rootDirectory = "/"
         }
-      },
-      {
-        "name": "offline",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_offline.file_system_id}",
-          "rootDirectory": "/"
+      }, {
+        name = "offline"
+        efsVolumeConfiguration = {
+          fileSystemId = var.efs_file_system_ids["offline"]
+          rootDirectory = "/"
         }
-      },
-      {
-        "name": "validation",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_validation.file_system_id}",
-          "rootDirectory": "/"
-        }
-      },
-      {
-        "name": "output",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_output.file_system_id}",
-          "rootDirectory": "/"
-        }
-      },
-      {
-        "name": "logs",
-        "efsVolumeConfiguration": {
-          "fileSystemId": "${data.aws_efs_file_system.aws_efs_logs.file_system_id}",
-          "rootDirectory": "/"
+      }, {
+        name = "logs"
+        efsVolumeConfiguration = {
+          fileSystemId = var.efs_file_system_ids["logs"]
+          rootDirectory = "/"
         }
       }
     ]
-  }
-  CONTAINER_PROPERTIES
+  })
+
   platform_capabilities = ["FARGATE"]
   propagate_tags        = true
   tags = { "job_definition": "${var.prefix}-clean-up" }
+}
+
+# IAM Role
+resource "aws_cloudwatch_log_group" "cw_log_group" {
+  name = "/aws/batch/job/${var.prefix}-clean-up/"
 }
