ad35d7192074d5fbc69bd66b29619d5c717d63ae
Josh Garde <joshgarde@gmail.com>
2025-05-20 05:28:56 -0700

    Parameterize Terraform Child Module (#2)
    * Initial terraform module refactor
    
    * Fix file system ids
    
    * Minor cleanup
    
    * Remove default_tags from child module
    
    * Lint resourceRequirements
    
    * Revert removal of terraform/provider blocks
    
    * Revert variables.tf in parent module
    
    * Switch execution role ARN + lint
    
    * Lint
    
    * default_tags padding
    
    * Fix volumes
    
    * Add missing filesystem ids
    
    * Fix reference to Input EFS data source
    
    ---------
    
    Co-authored-by: Nikki <17799906+nikki-t@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
- terraform {
+terraform {
   backend "s3" {
     encrypt = true
   }
@@ -17,10 +17,56 @@ provider "aws" {
   region  = var.aws_region
 }
 
+# Data sources
 data "aws_caller_identity" "current" {}
 
+data "aws_cloudwatch_log_group" "cw_log_group" {
+  name = "/aws/batch/job/${var.prefix}-clean-up/"
+}
+
+data "aws_efs_file_system" "input" {
+  creation_token = "${var.prefix}-input"
+}
+
+data "aws_efs_file_system" "flpe" {
+  creation_token = "${var.prefix}-flpe"
+}
+
+data "aws_efs_file_system" "moi" {
+  creation_token = "${var.prefix}-moi"
+}
+
+data "aws_efs_file_system" "diagnostics" {
+  creation_token = "${var.prefix}-diagnostics"
+}
+
+data "aws_efs_file_system" "offline" {
+  creation_token = "${var.prefix}-offline"
+}
+
+data "aws_efs_file_system" "validation" {
+  creation_token = "${var.prefix}-validation"
+}
+
+data "aws_efs_file_system" "output" {
+  creation_token = "${var.prefix}-output"
+}
+
+data "aws_efs_file_system" "logs" {
+  creation_token = "${var.prefix}-logs"
+}
+
+data "aws_iam_role" "job" {
+  name = "${var.prefix}-batch-job-role"
+}
+
+data "aws_iam_role" "exec" {
+  name = "${var.prefix}-ecs-exe-task-role"
+}
+
+# Local variables
 locals {
-  account_id = sensitive(data.aws_caller_identity.current.account_id)
+  account_id = data.aws_caller_identity.current.account_id
   default_tags = length(var.default_tags) == 0 ? {
     application : var.app_name,
     environment : lower(var.environment),
@@ -29,10 +75,22 @@ locals {
 }
 
 module "confluence-clean-up" {
-  source            = "./modules/clean"
-  app_name          = var.app_name
-  app_version       = var.app_version
-  aws_region        = var.aws_region
-  environment       = var.environment
-  prefix            = var.prefix
+  source = "./modules/clean"
+  app_name = var.app_name
+  app_version = var.app_version
+  aws_region = var.aws_region
+  efs_file_system_ids = {
+    input = data.aws_efs_file_system.input.file_system_id
+    flpe = data.aws_efs_file_system.flpe.file_system_id
+    moi = data.aws_efs_file_system.moi.file_system_id
+    diagnostics = data.aws_efs_file_system.diagnostics.file_system_id
+    offline = data.aws_efs_file_system.offline.file_system_id
+    validation = data.aws_efs_file_system.validation.file_system_id
+    output = data.aws_efs_file_system.output.file_system_id
+    logs = data.aws_efs_file_system.logs.file_system_id
+  }
+  environment = var.environment
+  iam_execution_role_arn = data.aws_iam_role.exec.arn
+  iam_job_role_arn = data.aws_iam_role.job.arn
+  prefix = var.prefix
 }
